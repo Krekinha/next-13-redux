3b973c369fe6cf58b16965c4c458761e2e7548a2
Dionison <krekmg@gmail.com>
2023-02-23 12:56:22 -0300

    componentA

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,10 @@
-'use client';
-
-import styles from './page.module.css'
-
-import type { RootState } from './GlobalRedux/store';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, incrementByAmount } from './GlobalRedux/Features/counter/counterSlice';
+import styles from "./page.module.css";
+import { ComponentA } from "@/components/ComponentA";
 
 export default function Home() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
     <main className={styles.main}>
-      <button 
-        className={styles.button}
-        onClick={() => dispatch(increment())}
-      >Increment</button>
-      <span>{count}</span>
-      <button 
-        className={styles.button}
-        onClick={() => dispatch(decrement())}
-      >Decrement</button>
-      <button 
-        className={styles.button}
-        onClick={() => dispatch(incrementByAmount(2))}
-      >Increment by 2</button>
+      <ComponentA />
     </main>
-  )
+  );
 }
